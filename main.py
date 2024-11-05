cb308249ebf06e09d01f9cdeb49cf552b42268f8
Ted <58144503+BUSH222@users.noreply.github.com>
2024-11-05 16:20:49 +0300

    Initial commit
    Co-authored-by: kust <VaDKustiK@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return "this works"
+
+@app.route('/based')
+def based():
+    return render_template('based.html')
+
+
 if __name__ == '__main__':
-    pass  # Do something
+    app.run(host='localhost', port=5000, debug=True)
